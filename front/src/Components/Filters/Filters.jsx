5b5b83c196edc5f45c2ee1a9e25c1587e057f626
valenpe <valentinaperez230701@gmail.com>
2023-09-30 14:57:20 -0300

    front 5

--- front/src/Components/Filters/Filters.jsx
+++ front/src/Components/Filters/Filters.jsx
@@ -1,21 +1,25 @@
-const orderPokes = require("../../Redux/actions")
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { orderPokes } from "../../Redux/actions";
+
 
 
 const Filters = () => {
 
     const dispatch = useDispatch();
-    const ordePoke = useSelector(state => state.myPokemons);
 
     useEffect(() => {
         dispatch(orderPokes())
     }, []);
 
+    const handleChange = (event) => {
+        const orderType = event.target.value;
+        dispatch(orderPokes(orderType))
+    }
+
     return (
         <div>
-          <select onChange={ordePoke}>
+          <select onChange={handleChange}>
               <option value="A">ASCENDENTE</option>
               <option value="D">DESCENDENTE</option>
           </select>
--- front/src/Components/Nav/Nav.jsx
+++ front/src/Components/Nav/Nav.jsx
@@ -1,9 +1,10 @@
 import SearchBar from "../SearchBar/SearchBar";
-
+import Filters from "../Filters/Filters";
 const Nav = () => {
     return(
         <div>
            <SearchBar/>
+           <Filters/>
         </div>
     )
 }
