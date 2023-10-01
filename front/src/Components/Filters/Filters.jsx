155bd3bc8a341ddc31e0c0c319f0e4071d2e01e6
valenpe <valentinaperez230701@gmail.com>
2023-10-01 01:30:52 -0300

    front 6

--- front/src/Components/Filters/Filters.jsx
+++ front/src/Components/Filters/Filters.jsx
@@ -1,27 +1,29 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { orderPokes } from "../../Redux/actions";
 
 
 
 const Filters = () => {
-
+    const pokemons = useSelector(state => state.myPokemons)
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(orderPokes())
-    }, []);
 
     const handleChange = (event) => {
         const orderType = event.target.value;
-        dispatch(orderPokes(orderType))
+        dispatch(orderPokes(orderType, pokemons))
     }
+    //orderType: son los tipos de ordenamiento
+    //pokemons: mis pokemones del estado global
 
     return (
         <div>
           <select onChange={handleChange}>
-              <option value="A">ASCENDENTE</option>
-              <option value="D">DESCENDENTE</option>
+              <option value="A">ASC</option>
+              <option value="D">DES</option>
+              <option value="ALF-ASC">ALF ASC</option>
+              <option value="ALF-DES">ALF DES</option>
+              <option value="ASC-ATTCK">ATTACK ASC</option>
+              <option value="DES-ATTCK">ATTACK ASC</option>
+            
           </select>
         </div>
     )
