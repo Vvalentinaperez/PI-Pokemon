53df72f2e1a7fcdf206e814f375ae69b887af255
valenpe <valentinaperez230701@gmail.com>
2023-10-03 22:26:14 -0300

    front 10

--- front/src/Components/Form/Form.jsx
+++ front/src/Components/Form/Form.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 const Form = () => {
 
   const navigate = useNavigate();
+  
 
   const [ pokemons, setPokemons ] = useState({
     name: "",
@@ -69,31 +70,31 @@ const Form = () => {
             <label htmlFor="name">Name: </label>
             <input type="text" name="name" placeholder="Name" value={pokemons.name} onChange={handleChange}/>{error.name && <p style={{color: "red"}}>{error.name}</p>}
           <hr/>
-            <label htmlFor="image">IMAGE: </label>
-            <input type="text" name="image" accept="image/*" placeholder="Image" value={pokemons.image} onChange={handleChange} /> {error.image && <p style={{color: "red"}}>{error.image}</p>}
+            <label htmlFor="image">Image: </label>
+            <input type="text" name="image" accept="image/*" placeholder="Image" value={pokemons.image} onChange={handleChange} /> {error?.image && <p style={{color: "red"}}>{error.image}</p>}
           <hr/>
-            <label htmlFor="types">TYPES: </label>
+            <label htmlFor="types">Types: </label>
             <input type="text" name="types" placeholder="Types" value={pokemons.types} onChange={handleChange} />{error.types && <p style={{color: "red"}}>{error.types}</p>}
           <hr/>
-            <label htmlFor="life">LIFE: </label>
+            <label htmlFor="life">Life: </label>
             <input type="number" name="life" min="0" max="200"value={pokemons.life} onChange={handleChange}/>{error.life && <p style={{color: "red"}}>{error.life}</p>}
           <hr/>
-            <label htmlFor="attack">ATTACK: </label>
+            <label htmlFor="attack">Attack: </label>
             <input type="number" name="attack" min="0" max="200" value={pokemons.attack}  onChange={handleChange}/>{error.attack && <p style={{color: "red"}}>{error.attack}</p>}
           <hr/>
-            <label htmlFor="defense">DEFENSE: </label>
+            <label htmlFor="defense">Defense: </label>
             <input type="number" name="defense" min="0" max="200" value={pokemons.defense} onChange={handleChange}/>{error.defense && <p style={{color: "red"}}>{error.defense}</p>}
           <hr/>
-            <label htmlFor="speed">SPEED: </label>
+            <label htmlFor="speed">Speed: </label>
             <input type="number" name="speed" min="0" max="200" value={pokemons.speed} onChange={handleChange}/>{error.speed && <p style={{color: "red"}}>{error.speed}</p>}
           <hr/>
-            <label htmlFor="weight">WEIGHT: </label>
+            <label htmlFor="weight">Weight: </label>
             <input type="number" name="weight" min="0" max="200" value={pokemons.weight} onChange={handleChange}/>{error.weight && <p style={{color: "red"}}>{error.weight}</p>}
           <hr/>
-            <label htmlFor="height">HEIGHT: </label>
+            <label htmlFor="height">Height: </label>
             <input type="number" name="height" min="0" max="200" value={pokemons.height}  onChange={handleChange}/>{error.height && <p style={{color: "red"}}>{error.height}</p>}
           <hr/>
-            <button type="submit">SUBMIT</button>
+            <button type="submit" disabled={!pokemons.name || !pokemons.image || !pokemons.types || !pokemons.life || !pokemons.attack || !pokemons.defense || !pokemons.speed || !pokemons.weight || !pokemons.height || error.name ||  error.types || error.life || error.attack || error.defense || error.speed || error.weight || error.height}>SUBMIT</button>
         </form>
     )
 }
--- front/src/Components/Validation/validation.js
+++ front/src/Components/Validation/validation.js
@@ -4,38 +4,56 @@ const validation = (pokemons) => {
 
   if(!pokemons.name){
     error.name = "Debe ingresar un name";
+  }else{
+    error.name = "";
   }
 
   if(!pokemons.image || !regex.test(pokemons.image)){
     error.image = "Debe ingresar una imagen";
+  }else{
+    error.image = "";
   }
 
   if(!pokemons.types){
     error.types = "Debe ingresar por lo menos un tipo";
+  }else{
+    error.types = "";
   }
 
   if(pokemons.life === 0){
     error.life = "Debe ingresar un numero de vida";
+  }else{
+    error.life = "";
   }
 
   if(pokemons.attack === 0){
     error.attack = "Debe ingresar un numero para el ataque";
+  }else{
+    error.attack = "";
   }
 
   if(pokemons.defense === 0){
     error.defense = "Debe ingresar un numero para la defensa";
+  }else{
+    error.defense = "";
   }
 
   if(pokemons.speed === 0){
     error.speed = "Debe ingresar un numero para la velocidad";
+  }else{
+    error.speed = "";
   }
 
   if(pokemons.weight === 0){
     error.weight = "Debe ingresar un numero para el peso";
+  }else{
+    error.weight = "";
   }
 
   if(pokemons.height === 0){
     error.height = "Debe ingresar un numero para la altura";
+  }else{
+    error.height = "";
   }
 
   return error;
