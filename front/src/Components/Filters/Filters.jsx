8d623cfa29b5abd8005574b5c20377f6898a62a2
valenpe <valentinaperez230701@gmail.com>
2023-10-04 22:51:53 -0300

    front xxx

--- front/src/Components/Filters/Filters.jsx
+++ front/src/Components/Filters/Filters.jsx
@@ -14,6 +14,7 @@ const Filters = () => {
     const handleChange = (event) => {
         const orderType = event.target.value;
         dispatch(orderPokes(orderType, pokemons))
+        console.log(orderPokes)
     }
     
     const handleChangeOrigin = (event) => {
