2f82c86ac216c52a78a61a221224fdfc556f38ae
valenpe <valentinaperez230701@gmail.com>
2023-10-07 16:47:02 -0300

    primeros diseños

--- front/src/Components/LandingPage/LandingPage.jsx
+++ front/src/Components/LandingPage/LandingPage.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom"
+import poke from '../util/pokelanding.png'
+
 import './LandingPage.css'
 
 const LandingPage = () => {
     return (
-          <div className="button.landing">
-            <button >
-              <Link to="/home">HOME</Link>
-            </button>
-        </div>
+      <div className="background_landing">
+           <img src={poke} className="pokemon_landing"/>
+          
+             <div >
+               <button className="button_landing"> 
+                <Link to="/home">HOME</Link>
+               </button>
+             </div>
+      </div>
+         
     )
 }
 
